4ded74fe8cde68bdf3bcbe8951e95c44abbd95e7
Bridget Sharitt <bridget@sharitt.com>
2023-05-19 17:05:27 -0500

    add back up extension

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ pandoc -f markdown -t html resume.md -o output/index.html
 pandoc -f markdown resume.md -o output/resume.pdf
 pandoc -f markdown -t odt resume.md -o output/resume.odt
 
-sed -i '' '1s/^/<a href="resume.docx">DOCX<\/a> <a href="resume.pdf">PDF<\/a> <a href="resume.odt">ODT<\/a> <a href="https:\/\/github.com\/bsharitt\/resume">Source<\/a>/' output/index.html
+sed -i .bak '1s/^/<a href="resume.docx">DOCX<\/a> <a href="resume.pdf">PDF<\/a> <a href="resume.odt">ODT<\/a> <a href="https:\/\/github.com\/bsharitt\/resume">Source<\/a>/' output/index.html
+
+rm output/index.html.bak
 
 if [ -d "/home/bsharitt/public_html/resume" ]
 then
